1b12ca1fee91dbb7c93304e96598af4f883339f2
hugofolloni <hugofollogua07@gmail.com>
2023-11-16 04:55:20 -0300

    :rocket: Adding files

---
+++ README.md
@@ -0,0 +1,2 @@
+# spotify-shuffle
+A way to customzie your listening order on Spotify's shuffle mode. We expect to get new playlist's songs on top of the queue, and that's what this program do.
---
+++ spotify-shuffle.py
@@ -0,0 +1,954 @@
+import random
+
+data = [ {
+    "track" : {
+      "artists" : [ {
+        "name" : "$uicideboy$"
+      } ],
+      "new" : False,
+      "name" : "Kill Yourself (Part IV)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "$uicideboy$"
+      } ],
+      "new" : False,
+      "name" : "Matte Black"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "$uicideboy$"
+      } ],
+      "new" : False,
+      "name" : "1000 Blunts"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "$uicideboy$"
+      } ],
+      "new" : False,
+      "name" : "No Matter Which Direction I'm Going In, I Never Chase These Hoes"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "$uicideboy$"
+      }, {
+        "name" : "Ghostemane"
+      } ],
+      "new" : False,
+      "name" : "Bloody 98 (Feat. Ghostemane)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "1nonly"
+      }, {
+        "name" : "Freddie Dredd"
+      } ],
+      "new" : False,
+      "name" : "POPTHATRUNK"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Alice In Chains"
+      } ],
+      "new" : False,
+      "name" : "Would? (2022 Remaster)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Arctic Monkeys"
+      } ],
+      "new" : False,
+      "name" : "Do Me a Favour"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Avenged Sevenfold"
+      } ],
+      "new" : False,
+      "name" : "So Far Away"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Avenged Sevenfold"
+      } ],
+      "new" : False,
+      "name" : "Tonight the World Dies"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Avenged Sevenfold"
+      } ],
+      "new" : False,
+      "name" : "Unholy Confessions"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Avenged Sevenfold"
+      } ],
+      "new" : False,
+      "name" : "I Won't See You Tonight Part 1"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bad Omens"
+      } ],
+      "new" : False,
+      "name" : "Glass Houses"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bad Omens"
+      } ],
+      "new" : False,
+      "name" : "Like A Villain"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bad Omens"
+      } ],
+      "new" : False,
+      "name" : "Just Pretend"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bad Omens"
+      } ],
+      "new" : False,
+      "name" : "The Grey"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Band of Horses"
+      } ],
+      "new" : False,
+      "name" : "The Funeral"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Basement"
+      } ],
+      "new" : False,
+      "name" : "Covet"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Beach Weather"
+      } ],
+      "new" : False,
+      "name" : "Sex, Drugs, Etc."
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bon Iver"
+      }, {
+        "name" : "St. Vincent"
+      } ],
+      "new" : False,
+      "name" : "Rosyln"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bring Me The Horizon"
+      }, {
+        "name" : "BABYMETAL"
+      } ],
+      "new" : False,
+      "name" : "Kingslayer (feat. BABYMETAL)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bring Me The Horizon"
+      } ],
+      "new" : False,
+      "name" : "Doomed"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Bring Me The Horizon"
+      } ],
+      "new" : False,
+      "name" : "Don't Go"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Catfish and the Bottlemen"
+      } ],
+      "new" : False,
+      "name" : "Longshot"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Charlie Brown Jr."
+      } ],
+      "new" : False,
+      "name" : "Lugar Ao Sol"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Charlie Brown Jr."
+      } ],
+      "new" : False,
+      "name" : "Como Tudo Deve Ser"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Chase Atlantic"
+      } ],
+      "new" : False,
+      "name" : "OHMAMI"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Chase Atlantic"
+      } ],
+      "new" : False,
+      "name" : "The Walls"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Chase Atlantic"
+      }, {
+        "name" : "GOON DES GARCONS*"
+      } ],
+      "new" : False,
+      "name" : "Consume (feat. Goon Des Garcons)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Chase Atlantic"
+      } ],
+      "new" : False,
+      "name" : "LIKE A ROCKSTAR"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Clairo"
+      } ],
+      "new" : False,
+      "name" : "Bags"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Conan Gray"
+      } ],
+      "new" : False,
+      "name" : "Comfort Crowd"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Dance Gavin Dance"
+      } ],
+      "new" : False,
+      "name" : "We Own The Night"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "A Day To Remember"
+      } ],
+      "new" : False,
+      "name" : "If It Means A Lot To You"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "Fist"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "Be Quiet and Drive (Far Away)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "Please Please Please Let Me Get What I Want - 2005 Remaster"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "Minerva"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "Sextape"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "Phantom Bride"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "What Happened to You?"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "The Spell of Mathematics"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Deftones"
+      } ],
+      "new" : False,
+      "name" : "Change (In the House of Flies)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "EDEN"
+      } ],
+      "new" : False,
+      "name" : "Wake Up"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "EDEN"
+      } ],
+      "new" : False,
+      "name" : "isohel"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "EDEN"
+      } ],
+      "new" : False,
+      "name" : "take care"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Far Caspian"
+      } ],
+      "new" : False,
+      "name" : "Let's Go Outside"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Foo Fighters"
+      } ],
+      "new" : False,
+      "name" : "Everlong"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Freddie Dredd"
+      } ],
+      "new" : False,
+      "name" : "Cha Cha"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Freddie Dredd"
+      } ],
+      "new" : False,
+      "name" : "GTG"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "HAARPER"
+      }, {
+        "name" : "Freddie Dredd"
+      } ],
+      "new" : False,
+      "name" : "Wtf U Mean"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "John Mayer"
+      } ],
+      "new" : False,
+      "name" : "Neon"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Joji"
+      } ],
+      "new" : False,
+      "name" : "MODUS"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Joji"
+      } ],
+      "new" : False,
+      "name" : "777"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Joji"
+      } ],
+      "new" : False,
+      "name" : "YUKON (INTERLUDE)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Joji"
+      } ],
+      "new" : False,
+      "name" : "1AM FREESTYLE"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Kendrick Lamar"
+      }, {
+        "name" : "Jay Rock"
+      } ],
+      "new" : False,
+      "name" : "Money Trees"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Lil Peep"
+      } ],
+      "new" : False,
+      "name" : "U Said"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Lil Peep"
+      } ],
+      "new" : False,
+      "name" : "IDGAF"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Lil Peep"
+      } ],
+      "new" : False,
+      "name" : "hellboy"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Lil Peep"
+      }, {
+        "name" : "rainy bear"
+      } ],
+      "new" : False,
+      "name" : "nuts"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Limp Bizkit"
+      } ],
+      "new" : False,
+      "name" : "Break Stuff"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Linkin Park"
+      } ],
+      "new" : False,
+      "name" : "Papercut"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Linkin Park"
+      } ],
+      "new" : False,
+      "name" : "Lying from You"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Linkin Park"
+      } ],
+      "new" : False,
+      "name" : "From the Inside"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "My Chemical Romance"
+      } ],
+      "new" : False,
+      "name" : "Dead!"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "My Chemical Romance"
+      } ],
+      "new" : False,
+      "name" : "Famous Last Words"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "My Chemical Romance"
+      } ],
+      "new" : False,
+      "name" : "Helena"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "My Chemical Romance"
+      } ],
+      "new" : False,
+      "name" : "I'm Not Okay (I Promise)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "The Neighbourhood"
+      } ],
+      "new" : False,
+      "name" : "A Little Death"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "The Neighbourhood"
+      } ],
+      "new" : False,
+      "name" : "The Beach"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Nirvana"
+      } ],
+      "new" : False,
+      "name" : "In Bloom"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Paramore"
+      } ],
+      "new" : False,
+      "name" : "Conspiracy"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Paramore"
+      } ],
+      "new" : False,
+      "name" : "Ignorance"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Pearl Jam"
+      } ],
+      "new" : False,
+      "name" : "State of Love and Trust"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Pearl Jam"
+      } ],
+      "new" : False,
+      "name" : "Black"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Phoebe Bridgers"
+      } ],
+      "new" : False,
+      "name" : "Motion Sickness"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Phoebe Bridgers"
+      } ],
+      "new" : False,
+      "name" : "Funeral"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Pierce The Veil"
+      } ],
+      "new" : False,
+      "name" : "Hell Above"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Pierce The Veil"
+      }, {
+        "name" : "Kellin Quinn"
+      } ],
+      "new" : False,
+      "name" : "King For A Day"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Pink Sweat$"
+      } ],
+      "new" : False,
+      "name" : "Cocaine"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Plain White T's"
+      } ],
+      "new" : False,
+      "name" : "Hey There Delilah - Bonus Track"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Polyphia"
+      }, {
+        "name" : "Sophia Black"
+      } ],
+      "new" : False,
+      "name" : "ABC (feat. Sophia Black)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Polyphia"
+      }, {
+        "name" : "Chino Moreno"
+      } ],
+      "new" : False,
+      "name" : "Bloodbath (feat. Chino Moreno)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "RAYE"
+      }, {
+        "name" : "070 Shake"
+      } ],
+      "new" : False,
+      "name" : "Escapism. - Sped Up"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Slipknot"
+      } ],
+      "new" : False,
+      "name" : "Snuff"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Slipknot"
+      } ],
+      "new" : False,
+      "name" : "Vermilion"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Slipknot"
+      } ],
+      "new" : False,
+      "name" : "Unsainted"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "The Smiths"
+      } ],
+      "new" : False,
+      "name" : "Ask - 2011 Remaster"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "The Smiths"
+      } ],
+      "new" : False,
+      "name" : "I Know It's Over - 2011 Remaster"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "The Smiths"
+      } ],
+      "new" : False,
+      "name" : "Bigmouth Strikes Again - 2011 Remaster"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Soundgarden"
+      } ],
+      "new" : False,
+      "name" : "Black Hole Sun"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Superheaven"
+      } ],
+      "new" : False,
+      "name" : "Youngest Daughter"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Tate McRae"
+      } ],
+      "new" : False,
+      "name" : "greedy"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Taylor Swift"
+      }, {
+        "name" : "Bon Iver"
+      } ],
+      "new" : False,
+      "name" : "evermore (feat. Bon Iver)"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Taylor Swift"
+      } ],
+      "new" : False,
+      "name" : "Cruel Summer"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Terno Rei"
+      } ],
+      "new" : False,
+      "name" : "Dias da Juventude"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Terno Rei"
+      } ],
+      "new" : False,
+      "name" : "Sorte Ainda"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Terno Rei"
+      } ],
+      "new" : False,
+      "name" : "Medo"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Terno Rei"
+      } ],
+      "new" : False,
+      "name" : "Amor-Perfeito"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Loathe"
+      } ],
+      "new" : True,
+      "name" : "Is It Really You?"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Sleep Token"
+      } ],
+      "new" : True,
+      "name" : "Is It Really You?"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Sleep Token"
+      } ],
+      "new" : True,
+      "name" : "Take Me Back To Eden"
+    }
+  },{
+    "track" : {
+      "artists" : [ {
+        "name" : "Pierce The Veil"
+      } ],
+      "new" : True,
+      "name" : "A Match Into Water"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Sleeping With Sirens"
+      } ],
+      "new" : True,
+      "name" : "If You Can't Hang"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "The Weeknd"
+      } ],
+      "new" : True,
+      "name" : "One Of The Girls"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Lil Peep"
+      } ],
+      "new" : True,
+      "name" : "absolute in doubt"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Lil Peep"
+      } ],
+      "new" : True,
+      "name" : "your favorite dress"
+    }
+  }, {
+    "track" : {
+      "artists" : [ {
+        "name" : "Lil Peep"
+      } ],
+      "new" : True,
+      "name" : "live forever"
+    }
+  },
+  ]
+
+def print_items(data):
+    for i in range(len(data)):
+        track = data[i]['track']
+        name = track['name']
+        artists = track['artists']
+        main_artist = artists[0]['name']
+        label = ""
+        if track['new']:
+            label = "NEW"
+        print(" {:^4} | {:^80} | {:80} | {:10}".format(i + 1, name, main_artist, label))
+
+def create_list(size):
+    new_list = []
+    for i in range(size):
+        new_list.append("empty")
+    return  new_list
+
+def getting_priorities(data):
+    prio_list = []
+    for item in data:
+         if item['track']['new'] == True:
+             prio_list.append(item)
+             data.remove(item)
+    return prio_list, data
+
+def set_items(item, new_list, position, playlist_size):
+    while new_list[position] != "empty":
+            position = (position + 1) % playlist_size
+    new_list[position] = item
+    return new_list
+    
+def get_list(data, new_list, PRIORITY, playlist_size):
+    prio_list, data = getting_priorities(data)
+    for item in prio_list:
+        position = random.randint(0, playlist_size - 1) % PRIORITY
+        PRIORITY += PRIORITY
+        new_list = set_items(item, new_list, position, playlist_size)
+    for item in data:
+        position = random.randint(0, playlist_size - 1)
+        new_list = set_items(item, new_list, position, playlist_size)
+    return new_list
+
+def __main__():
+    SIZE = len(data)
+    new_list = create_list(SIZE)
+    shuffled = get_list(data, new_list, 5, SIZE)
+    print_items(shuffled)
+
+__main__()
+
+## curl --request GET\ --url 'https://api.spotify.com/v1/playlists/2FBhNF1w3GCMdaNUwZcTs0/tracks?fields=items%28track%28name%2C+artists%28name%29%29%29'\--header'Authorization:Bearer {auth}'
